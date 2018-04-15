cfa2c3f3930c5941db489a216fe80375ee2ae1fa
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2018-04-15 21:45:32 +0300

    Add GUI elements for safety details.

--- ascom-safety/KomaSafetyMonitor/SetupDialogForm.cs
+++ ascom-safety/KomaSafetyMonitor/SetupDialogForm.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
+using System.IO;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
 using ASCOM.Utilities;
+using Newtonsoft.Json.Linq;
 
 namespace ASCOM.Komakallio
 {
@@ -51,6 +54,60 @@ namespace ASCOM.Komakallio
         private void InitUI()
         {
             serverAddressTextBox.Text = SafetyMonitor.serverAddress;
+        }
+
+        private void refreshDetailsButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                detailsListView.Clear();
+                refreshDetailsButton.Enabled = false;
+                var serverAddress = serverAddressTextBox.Text;
+                JObject values = getServerResponse(serverAddress);
+
+                foreach (JProperty item in values["details"])
+                {
+                    detailsListView.Items.Add(item.Name);
+                    detailsListView.Items[detailsListView.Items.Count - 1].Checked = true;
+                }
+            }
+            catch (Exception)
+            {
+                detailsListView.Clear();
+                MessageBox.Show("Could not connect to server!", "Connection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } finally
+            {
+                refreshDetailsButton.Enabled = true;
+            }
+        }
+
+        private static JObject getServerResponse(string serverAddress)
+        {
+            JObject values;
+            var request = WebRequest.Create(serverAddress) as HttpWebRequest;
+            request.Timeout = 1000;
+            using (var response = request.GetResponse() as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception(String.Format("Server error (HTTP {0}: {1}).",
+                        response.StatusCode,
+                        response.StatusDescription));
+                }
+
+                string jsonResponse;
+                using (var responseStream = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(responseStream, System.Text.Encoding.UTF8))
+                    {
+                        jsonResponse = reader.ReadToEnd();
+                    }
+                }
+
+                values = JObject.Parse(jsonResponse);
+            }
+
+            return values;
         }
     }
 }
