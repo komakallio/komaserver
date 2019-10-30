85a318e2f375ecac1f9c2038a2a5ecc4b4ee7c29
Jari Saukkonen <jari.saukkonen@taika.fi>
2019-10-30 21:34:46 +0200

    Update safety regularly to redis

--- safety/server.js
+++ safety/server.js
@@ -21,6 +21,8 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+const _ = require('underscore');
+const request = require('request');
 const express = require('express');
 const expressLogging = require('express-logging');
 const logger = require('logops');
@@ -54,109 +56,114 @@ app.use(expressLogging(logger));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/safety', function(req, res) {
+function calculateSafety() {
+    return new Promise((resolve, reject) => {
+        return Promise.join(
+            redisClient.zrevrangeAsync('ptu', 0 ,0),
+            redisClient.zrevrangeAsync('rain', 0, 0),
+            redisClient.zrevrangeAsync('raintrigger', 0, 0),
+            redisClient.zrevrangeAsync('radar', 0, 0),
+            redisClient.zrevrangeAsync('ups', 0, 0),
+            redisClient.zrevrangeAsync('interior', 0, 0),
 
-    Promise.join(
-        redisClient.zrevrangeAsync('ptu', 0 ,0),
-        redisClient.zrevrangeAsync('rain', 0, 0),
-        redisClient.zrevrangeAsync('raintrigger', 0, 0),
-        redisClient.zrevrangeAsync('radar', 0, 0),
-        redisClient.zrevrangeAsync('ups', 0, 0),
-        redisClient.zrevrangeAsync('interior', 0, 0),
+            function(...replies) {
+                if (replies.find(reply => reply.length === 0)) {
+                    logger.error('Reading Redis failed');
+                    return reject('Reading redis failed');
+                }
 
-        function(...replies) {
-            if (replies.find(reply => reply.length === 0)) {
-                logger.error('Reading Redis failed');
-                res.status(500).send('{"error":"Reading redis failed"}');
-                return;
-            }
+                const ptu = JSON.parse(replies[0][0]),
+                      rain = JSON.parse(replies[1][0]),
+                      raintrigger = JSON.parse(replies[2][0]),
+                      radar = JSON.parse(replies[3][0]),
+                      ups = JSON.parse(replies[4][0]),
+                      interior = JSON.parse(replies[5][0]);
 
-            var ptu = JSON.parse(replies[0][0]),
-                rain = JSON.parse(replies[1][0]),
-                raintrigger = JSON.parse(replies[2][0]),
-                radar = JSON.parse(replies[3][0]),
-                ups = JSON.parse(replies[4][0]),
-                interior = JSON.parse(replies[5][0]);
+                const btemp = ptu.PTU.Temperature.Ambient[0] > -20;
+                const braintrigger = raintrigger.RainTrigger.Rain == 0;
+                const brainintensity = rain.Rain.Rain.Intensity[0] == 0;
+                const bradar10 = radar.Radar["10km"][0] < 0.5;
+                const bradar30 = radar.Radar["30km"][0] < 0.5;
+                const bradar50 = radar.Radar["50km"][0] < 0.5;
+                const bsun = SunCalc.getPosition(new Date(), latitude, longitude).altitude*180/Math.PI < -5;
+                const bupscharge = ups.UPS.BCHARGE[0] >= 50;
+                const benclosuretemp = interior.Interior.EnclosureTemp[0] > -15;
 
-            var btemp = ptu.PTU.Temperature.Ambient[0] > -20;
-            var braintrigger = raintrigger.RainTrigger.Rain == 0;
-            var brainintensity = rain.Rain.Rain.Intensity[0] == 0;
-            var bradar10 = radar.Radar["10km"][0] < 0.5;
-            var bradar30 = radar.Radar["30km"][0] < 0.5;
-            var bradar50 = radar.Radar["50km"][0] < 0.5;
-            var bsun = SunCalc.getPosition(new Date(), latitude, longitude).altitude*180/Math.PI < -5;
-            var bupscharge = ups.UPS.BCHARGE[0] >= 50;
-            var benclosuretemp = interior.Interior.EnclosureTemp[0] > -15;
+                const safe = btemp && braintrigger && brainintensity && bsun && bradar30 && bupscharge && benclosuretemp;
+                const safetyDetails = {
+                    temperature: { value: ptu.PTU.Temperature.Ambient[0], safe: btemp },
+                    rainintensity: { value: rain.Rain.Rain.Intensity[0], safe: brainintensity },
+                    raintrigger: { value: raintrigger.RainTrigger.Rain, safe: braintrigger },
+                    rainradar10km: { value: radar.Radar["10km"][0], safe: bradar10 },
+                    rainradar30km: { value: radar.Radar["30km"][0], safe: bradar30 },
+                    rainradar50km: { value: radar.Radar["50km"][0], safe: bradar50 },
+                    sunaltitude: { value: roundTo(SunCalc.getPosition(new Date(), latitude, longitude).altitude*180/Math.PI, 2), safe: bsun },
+                    moonaltitude: { value: roundTo(SunCalc.getMoonPosition(new Date(), latitude, longitude).altitude*180/Math.PI, 2), safe: true },
+                    upscharge: { value: ups.UPS.BCHARGE[0], safe: bupscharge },
+                    enclosuretemp: { value: interior.Interior.EnclosureTemp[0], safe: benclosuretemp }
+                };
 
-            var safe = btemp && braintrigger && brainintensity && bsun && bradar30 && bupscharge && benclosuretemp;
-            var safetyDetails = {
-                temperature: { value: ptu.PTU.Temperature.Ambient[0], safe: btemp },
-                rainintensity: { value: rain.Rain.Rain.Intensity[0], safe: brainintensity },
-                raintrigger: { value: raintrigger.RainTrigger.Rain, safe: braintrigger },
-                rainradar10km: { value: radar.Radar["10km"][0], safe: bradar10 },
-                rainradar30km: { value: radar.Radar["30km"][0], safe: bradar30 },
-                rainradar50km: { value: radar.Radar["50km"][0], safe: bradar50 },
-                sunaltitude: { value: roundTo(SunCalc.getPosition(new Date(), latitude, longitude).altitude*180/Math.PI, 2), safe: bsun },
-                moonaltitude: { value: roundTo(SunCalc.getMoonPosition(new Date(), latitude, longitude).altitude*180/Math.PI, 2), safe: true },
-                upscharge: { value: ups.UPS.BCHARGE[0], safe: bupscharge },
-                enclosuretemp: { value: interior.Interior.EnclosureTemp[0], safe: benclosuretemp }
-            };
+                if (!safe && lastSafe) {
+                    logger.info('Condition now unsafe: ' + JSON.stringify(safetyDetails));
+                }
+                lastSafe = safe;
 
-            if (!safe && lastSafe) {
-                logger.info('Condition now unsafe: ' + JSON.stringify(safetyDetails));
-            }
-            lastSafe = safe;
+                var data = {
+                    safe: safe,
+                    details: safetyDetails
+                };
 
-            var data = {
-                safe: safe,
-                details: safetyDetails
-            };
-            lastData = data;
-            res.json(data);
-        }
-    ).catch(function(err) {
-        logger.error('Reading Redis failed', err);
-        res.status(500).send('{"error":"Reading redis failed: ' + err +  '"}');
+                lastData = data;
+                return resolve(data);
+              }
+        ).catch(function(err) {
+            logger.error('Reading Redis failed', err);
+            return reject('Reading redis failed: ' + err);
+        });
     });
+}
+
+app.get('/safety', function(req, res) {
+    calculateSafety().then(
+        (data) => res.json(data),
+        (error) => res.status(500).send(`{"error":"%{error}"}`)
+    );
 });
 
 function safetyReporter() {
-    if (!lastData) {
-        setTimeout(safetyReporter, 1000);
-        return;
-    }
+    calculateSafety()
+        .finally(() => setTimeout(safetyReporter, 1000))
+        .then((safetyData) => {
+            const data = {
+                'Type': 'Safety',
+                'Safety': {
+                    'Safe': safetyData.safe,
+                    'Details': {
+                      'Temperature': safetyData.details.temperature.safe,
+                      'RainTrigger': safetyData.details.raintrigger.safe,
+                      'RainIntensity': safetyData.details.rainintensity.safe,
+                      'Radar': safetyData.details.rainradar30km.safe,
+                      'SunAltitude': safetyData.details.sunaltitude.safe,
+                      'UPSCharge': safetyData.details.upscharge.safe,
+                      'EnclosureTemp': safetyData.details.enclosuretemp.safe
+                  }
+                }
+            };
 
-    const data = {
-        'Type': 'Safety',
-        'Safety': {
-            'Safe': lastData.safe,
-            'Details': {
-              'Temperature': lastData.details.temperature.safe,
-              'RainTrigger': lastData.details.raintrigger.safe,
-              'RainIntensity': lastData.details.rainintensity.safe,
-              'Radar': lastData.details.rainradar30km,
-              'SunAltitude': lastData.details.sunaltitude.safe,
-              'UPSCharge': lastData.details.upscharge.safe,
-              'EnclosureTemp': lastData.details.enclosuretemp.safe
-          }
-        }
-    };
-
-    if (!_.isEqual(data, lastSafetyData) || (Date.now() - lastSafetyReportTime) > 300*1000) {
-        lastSafetyReportTime = Date.now();
-        request.post({
-            url: 'http://localhost:9001/api',
-            body: data,
-            json: true
-        }, function (error, response) {
-            if (!response || response.statusCode != 200) {
-                logger.warn(error);
+            if (!_.isEqual(data, lastSafetyData) || (Date.now() - lastSafetyReportTime) > 300*1000) {
+                lastSafetyReportTime = Date.now();
+                request.post({
+                    url: 'http://localhost:9001/api',
+                    body: data,
+                    json: true
+                }, function (error, response) {
+                    if (!response || response.statusCode != 200) {
+                        logger.warn(error);
+                    }
+                    lastSafetyData = data;
+                });
             }
-            lastSafetyData = data;
-        });
-    }
-
-    setTimeout(safetyReporter, 1000);
+      });
 }
 
 var server = app.listen(9002, function() {
